7c80cadba1e10deb96669d3792dd37f25eedf280
Matt <mrrdale@gmail.com>
2024-11-13 09:49:06 -0800

    Removed unnecessary files to optimize deployment

--- convert_model.py
+++
@@ -1,7 +0,0 @@
-from tensorflow.keras.models import load_model
-
-# Load the existing .h5 model
-model = load_model('goat_sex_model.h5')
-
-# Save it in the SavedModel format
-model.save('goat_sex_model', save_format='tf')
--- test_model_load.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-print("TensorFlow Version:", tf.__version__)
-print("Keras Version:", tf.keras.__version__)
-
-try:
-    model = tf.keras.models.load_model('C:/Users/mrrda/Documents/goat_project/goat_sex_model.h5')
-    print("Model loaded successfully")
-except Exception as e:
-    print("Error loading model:", str(e))
